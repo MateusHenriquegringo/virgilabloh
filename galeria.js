23bcfab1018deac897d39bee10d91907b0c85b88
Mateus Henrique Derossi <henriquemateus2502@gmail.com>
2023-06-18 16:34:51 -0300

    galeria-add

--- galeria.js
+++ galeria.js
@@ -14,54 +14,21 @@ moreImagesList.forEach(function(moreImages) {
   });
 });
 
-/*
-// Retrieve references to all the spans
-var spans = document.getElementsByTagName('span');
-// Loop through each span
-for (var i = 0; i < spans.length; i++) {
-  // Attach click event listener to each span
-  spans[i].addEventListener('click', function() {
-    // Retrieve the corresponding content element
-    var contentId = this.id + '-content';
-    var content = document.getElementById(contentId);
-    
-    // Remove the 'hide' class from the content element
-    content.classList.remove('hide');
-  });
+let spans = document.querySelectorAll('#conteiner span')
+let ids = []
+for (let i = 0; i<spans.length; i++){
+  let id = spans[i].getAttribute('id')
+  ids.push(id)
 }
-*/
-let architecture = document.getElementById('architecture')
-let artwork = document.getElementById('artwork')
-let design = document.getElementById('design')
-let exhibition = document.getElementById('exhibition')
-let exhibitionContent = document.getElementById('exhibition-content')
-let designContent = document.getElementById('design-content')
-let artworkContent = document.getElementById('artwork-content')
-let architectureContent = document.getElementById('architecture-content')
 
-artwork.addEventListener('click',function(){
-    artworkContent.classList.remove('hide')
-    designContent.classList.add('hide')
-    architectureContent.classList.add('hide')
-    exhibitionContent.classList.add('hide')
+let contents = ids.map(elemento => elemento + '-content');
+console.log(contents)
 
-})
-design.addEventListener('click',function(){
-    artworkContent.classList.add('hide')
-    exhibitionContent.classList.add('hide')
-    architectureContent.classList.add('hide')
-    designContent.classList.remove('hide')
-})
-architecture.addEventListener('click',function(){
-    artworkContent.classList.add('hide')
-    designContent.classList.add('hide')
-    architectureContent.classList.remove('hide')
-    exhibitionContent.classList.add('hide')
-})
-exhibition.addEventListener('click',function(){
-    artworkContent.classList.add('hide')
-    designContent.classList.add('hide')
-    architectureContent.classList.add('hide')
-    exhibitionContent.classList.remove('hide')
-})
-
+spans.forEach((span,index) => {
+  span.addEventListener('click', () => {
+    for(let i = 0; i<contents.length; i++){
+      document.getElementById(contents[i]).classList.add('hide');
+    }
+    document.getElementById(contents[index]).classList.remove('hide');
+  });
+});
